723153f29ef1b24a2e4d79b03b3c4aaf117387f0
黎剑锋 <510194165@qq.com>
2019-07-24 23:44:52 +0800

    完成首页的静态页面

--- smallpig/src/App.js
+++ smallpig/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Switch,
+  withRouter,
+  Route,
+  Redirect
+} from 'react-router-dom';
+// import { Menu, Icon } from 'antd';
+
+import Home from './view/Home/Home';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      nav:[
+        {
+          name:'Home',
+          path:'./home',
+          icon:'home',
+          title:'首页'
+        },
+        {
+          name:'Lend',
+          path:'./lend',
+          icon:'home',
+          title:'借出'
+        },
+        {
+          name:'Mine',
+          path:'./mine',
+          icon:'user',
+          title:'我的'
+        },
+      ],
+      current:'Home'
+    }
+  }
+  render(){
+    let {nav}=this.state;
+
+    return (
+      <div className="App">
+      
+      <Route path='/home' component={Home}/>
+      <footer>
+          <nav>
+              <ul> 
+                <li className={"active"}>
+                   <a href={"/wechat/home/index"}> <i className={"icon ic-home"}></i> <span>首页</span> </a> 
+                </li>
+               <li>
+                  <a href={"/wechat/mark/marklist"}> <i className={"icon ic-list"}></i> <span>出借</span> </a> 
+                </li>
+               <li> 
+                 <a href={"/wechat/user/index"}> <i className={"icon ic-user"}></i> <span>我的</span> </a> 
+              </li>
+              </ul> 
+          </nav> 
+      </footer>
+
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ smallpig/src/view/Home/Home.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react';
+import { Carousel } from 'antd';
+import './home.scss';
+class Home extends Component{
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+    render(){
+        return (
+            <div>
+                <section id={"top"} className={"top"} style={{display: "block"}}>
+                     <a href={"/?cut=web"}>点击返回电脑端</a> 
+                     <i id={"topClose"}></i>
+                </section>
+                <div>
+                    <Carousel autoplay>
+                        <div>
+                            <h3>
+                                <img src={require('../../assets/images/b1.png')} alt={''}/>
+                            </h3>
+                        </div>
+                        <div>
+                            <h3>
+                            <img src={require('../../assets/images/b2.png')} alt={''}/>
+                                
+                            </h3>
+                        </div>
+                        <div>
+                            <h3>
+                            <img src={require('../../assets/images/b3.jpg')} alt={''}/>
+                                
+                            </h3>
+                        </div>
+                    </Carousel>
+                </div>
+                <main>
+                     <div className={"data-show"}>
+                        <a href={"/Api/h5/safe"} className={"item left"}>
+                            <div className={"data"}>
+                                <span className={"title"}>已安全运营</span> 
+                                <p className={"value"}><span className={"accent"}>1606</span><span>天</span></p> 
+                            </div> 
+                                <p className={"logo"}> <img src={require('../../assets/images/safe.png')} style={{width:'1.52rem' ,height: '1.52rem'}} alt={''}/><span>安全保障</span> </p> 
+                        </a>
+                        <a href={"/Api/h5/data"} className={"item right"}> 
+                            <div className={"data"}>> 
+                                <span className={"title"}>累计成交额</span>
+                                <p className={"value"}> <span className={"accent"}>125</span><span>亿</span> <span className={"accent"}>3157</span><span>万</span> </p>
+                            </div> 
+                                <p className={"logo"}> <img src={require('../../assets/images/msg.png')} style={{width:'1.52rem' ,height: '1.52rem'}} alt={''}/>  <span>信息披露</span> </p>
+                        </a>
+                    </div>
+                    <ul className={"bdlist"}> 
+                        <li> 
+                            <a href={"/wechat/mark/details/id/62945"}> 
+                                <i className={"tag"}></i> 
+                                <p className={"title"}> <i className={"divider left-divider"}></i> <span>新手专享</span> <i className={"divider right-divider"}></i> </p> 
+                                <p className={"annual"}> <span>13.0</span> <span>%</span> </p> 
+                                <div className={"data"}> <p className={"balance"}> <span className={"title"}>剩余：</span> <span className={"value"}>0元</span> </p> 
+                                    <p className="term"> <span className={"title"}>期限：</span> <span className={"value"}>90天</span> </p> 
+                                </div> 
+                            </a>
+                            <a href={"/wechat/mark/details/id/62945"} className={"btn big bg disable"}>已满标</a> 
+                         </li>
+                         <li> 
+                            <a href={"/wechat/mark/details/id/62945"}> 
+                                <i className={"tag"}></i> 
+                                <p className={"title"}> <i className={"divider left-divider"}></i> <span>新手专享</span> <i className={"divider right-divider"}></i> </p> 
+                                <p className={"annual"}> <span>13.0</span> <span>%</span> </p> 
+                                <div className={"data"}> <p className={"balance"}> <span className={"title"}>剩余：</span> <span className={"value"}>0元</span> </p> 
+                                    <p className="term"> <span className={"title"}>期限：</span> <span className={"value"}>90天</span> </p> 
+                                </div> 
+                            </a>
+                            <a href={"/wechat/mark/details/id/62945"} className={"btn big bg disable"}>已满标</a> 
+                         </li>
+                         <li> 
+                            <a href={"/wechat/mark/details/id/62945"}> 
+                                <i className={"tag"}></i> 
+                                <p className={"title"}> <i className={"divider left-divider"}></i> <span>新手专享</span> <i className={"divider right-divider"}></i> </p> 
+                                <p className={"annual"}> <span>13.0</span> <span>%</span> </p> 
+                                <div className={"data"}> <p className={"balance"}> <span className={"title"}>剩余：</span> <span className={"value"}>0元</span> </p> 
+                                    <p className="term"> <span className={"title"}>期限：</span> <span className={"value"}>90天</span> </p> 
+                                </div> 
+                            </a>
+                            <a href={"/wechat/mark/details/id/62945"} className={"btn big bg disable"}>已满标</a> 
+                         </li>
+                       
+                    </ul> 
+                </main>
+                    
+                   
+                               
+            </div>
+        )
+    }
+}
+
+
+export default Home;
+
